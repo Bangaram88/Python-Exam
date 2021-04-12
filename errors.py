0cbd544eec0bdcc2a2853144361cfec35b13dcb8
Bangaram88 <jami.alekhya28798@gmail.com>
2021-04-12 13:06:35 +0530

    Add files via upload

---
+++ errors.py
@@ -0,0 +1,10 @@
+from flask import Response, jsonify
+
+def invalid_request() -> Response :
+    output = {"error":
+                {'message':'400 error: Invalid Request'}
+                }
+
+    response = jsonify({'result': output})
+    response.status_code = 400
+    return response
